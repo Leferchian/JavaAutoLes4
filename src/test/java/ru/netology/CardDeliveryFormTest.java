08a7ba52b3d0d8173ebe63eaa2d981df95ad4ce9
Egor Boyko <leferchian@gmail.com>
2022-11-30 00:44:34 +0300

    Fixed Month

--- src/test/java/ru/netology/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/CardDeliveryFormTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
+import java.util.Date;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryFormTest {
     @Test
